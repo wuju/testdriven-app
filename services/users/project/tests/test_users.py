5dc5c507be794403fb46908e1b344f9fd43d2374
Woo Joo Lee <wuju.zero@gmail.com>
2019-09-22 20:14:56 +1200

    Add single user test and feature

--- services/users/project/tests/test_users.py
+++ services/users/project/tests/test_users.py
@@ -5,6 +5,8 @@ import json
 import unittest
 
 from project.tests.base import BaseTestCase
+from project import db
+from project.api.models import User
 
 
 class TestUserService(BaseTestCase):
@@ -85,7 +87,39 @@ class TestUserService(BaseTestCase):
             self.assertEqual(response.status_code, 400)
             self.assertIn(
                 'Sorry. That email already exists.', data['message'])
-            self.assertIn('fail', data['status'])            
+            self.assertIn('fail', data['status'])
+
+    def test_single_user(self):
+        """Ensure get single user behaves correctly."""
+        user = User(username='michael', email='michael@mherman.org')
+        db.session.add(user)
+        db.session.commit()
+
+        with self.client:
+            response = self.client.get(f'/users/{user.id}')
+            data = json.loads(response.data.decode())
+            self.assertEqual(response.status_code, 200)
+            self.assertIn('michael', data['data']['username'])
+            self.assertIn('michael@mherman.org', data['data']['email'])
+            self.assertIn('success', data['status'])
+
+    def test_single_user_no_id(self):
+        """Ensure error is thrown if an id is not provided."""
+        with self.client:
+            response = self.client.get('/users/blah')
+            data = json.loads(response.data.decode())
+            self.assertEqual(response.status_code, 404)
+            self.assertIn('User does not exist', data['message'])
+            self.assertIn('fail', data['status'])
+
+    def test_single_user_incorrect_id(self):
+        """Ensure error is thrown if the id does not exist."""
+        with self.client:
+            response = self.client.get('/users/999')
+            data = json.loads(response.data.decode())
+            self.assertEqual(response.status_code, 404)
+            self.assertIn('User does not exist', data['message'])
+            self.assertIn('fail', data['status'])
 
 if __name__ == '__main__':
     unittest.main()
