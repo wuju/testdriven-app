9c441187a27bb2f8345bc6618e13c6e0d949e11f
Woo Joo Lee <wuju.zero@gmail.com>
2019-09-26 18:55:26 +1200

    Add create-react-app and allow CORS in users service

--- services/client/src/index.js
+++ services/client/src/index.js
@@ -1,12 +1,48 @@
-import React from 'react';
+import React, { Component } from 'react';  // new
 import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import * as serviceWorker from './serviceWorker';
+import axios from 'axios';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+import UsersList from './components/UserList';
 
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: https://bit.ly/CRA-PWA
-serviceWorker.unregister();
+
+// new
+class App extends Component {
+  constructor() {
+    super();
+    this.state = {
+      users: []
+    };
+  }
+
+  componentDidMount() {
+    this.getUsers();
+  }
+
+  getUsers() {
+    axios.get(`${process.env.REACT_APP_USERS_SERVICE_URL}/users`)
+      .then((res) => { this.setState({ users: res.data.data.users }); })
+      .catch((err) => { console.log(err); });
+  }
+
+  render() {
+    return (
+      <section className="section">
+        <div className="container">
+          <div className="columns">
+            <div className="column is-one-third">
+              <br />
+              <h1 className="title is-1">All Users</h1>
+              <hr /><br />
+              <UsersList users={this.state.users} />
+            </div>
+          </div>
+        </div>
+      </section>
+    )
+  }
+};
+
+ReactDOM.render(
+  <App />,
+  document.getElementById('root')
+);
