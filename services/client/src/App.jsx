59d67f97a1e7ee3fa286b848882200352b01a456
Woo Joo Lee <wuju.zero@gmail.com>
2019-09-30 19:32:55 +1300

    Add NavBar

--- services/client/src/App.jsx
+++ services/client/src/App.jsx
@@ -5,6 +5,7 @@ import { Route, Switch } from 'react-router-dom';
 import UsersList from './components/UsersList';
 import AddUser from './components/AddUser';
 import About from './components/About';
+import NavBar from './components/NavBar';
 
 
 class App extends Component {
@@ -14,6 +15,7 @@ class App extends Component {
       users: [],
       username: '',
       email: '',
+      title: 'TestDriven.io',
     };
     this.addUser = this.addUser.bind(this);
     this.handleChange = this.handleChange.bind(this);
@@ -51,33 +53,36 @@ class App extends Component {
 
   render() {
     return (
-      <section className="section">
-        <div className="container">
-          <div className="columns">
-            <div className="column is-half">
-              <br />
-              <Switch>
-                <Route exact path="/" render={() => (
-                  <div>
-                    <h1 className="title is-1">All Users</h1>
-                    <hr /><br />
-                    <AddUser
-                        username={this.state.username}
-                        email={this.state.email}
-                        addUser={this.addUser}
-                        handleChange={this.handleChange}
-                      />
-                    <br></br>
-                    <UsersList users={this.state.users} />
-                    <br/>
-                  </div>
-                )} />                
-              </Switch>
-              <Route exact path='/about' component={About}/>
+      <div>
+        <NavBar title={this.state.title} />
+        <section className="section">
+          <div className="container">
+            <div className="columns">
+              <div className="column is-half">
+                <br />
+                <Switch>
+                  <Route exact path="/" render={() => (
+                    <div>
+                      <h1 className="title is-1">All Users</h1>
+                      <hr /><br />
+                      <AddUser
+                          username={this.state.username}
+                          email={this.state.email}
+                          addUser={this.addUser}
+                          handleChange={this.handleChange}
+                        />
+                      <br></br>
+                      <UsersList users={this.state.users} />
+                      <br/>
+                    </div>
+                  )} />                
+                </Switch>
+                <Route exact path='/about' component={About}/>
+              </div>
             </div>
           </div>
-        </div>
-      </section>
+        </section>
+      </div>
     )
   }
 };
