8b8929a7ee8a3296209453f61c9ce34e235bda27
Woo Joo Lee <wuju.zero@gmail.com>
2019-09-30 21:05:41 +1300

    Wire up register and login to backend service

--- services/client/src/App.jsx
+++ services/client/src/App.jsx
@@ -24,6 +24,8 @@ class App extends Component {
     };
     this.addUser = this.addUser.bind(this);
     this.handleChange = this.handleChange.bind(this);
+    this.handleUserFormSubmit = this.handleUserFormSubmit.bind(this);
+    this.handleFormChange = this.handleFormChange.bind(this);
   }
 
   componentDidMount() {
@@ -56,6 +58,40 @@ class App extends Component {
     this.setState(obj);
   }
 
+  handleUserFormSubmit(event) {
+    event.preventDefault();
+    const formType = window.location.href.split('/').reverse()[0];
+    let data = {
+      email: this.state.formData.email,
+      password: this.state.formData.password,
+    };
+    if (formType === 'register') {
+      data.username = this.state.formData.username;
+    }
+    const url = `${process.env.REACT_APP_USERS_SERVICE_URL}/auth/${formType}`
+    axios.post(url, data)
+    .then((res) => {
+      this.clearFormState();
+      window.localStorage.setItem('authToken', res.data.auth_token);
+    })
+    .catch((err) => { console.log(err); });
+  }
+
+  handleFormChange(event) {
+    event.preventDefault();
+    const obj = this.state.formData;
+    obj[event.target.name] = event.target.value;
+    this.setState(obj);
+  }
+
+  clearFormState() {
+    this.setState({
+      formData: { username: '', email: '', password: ''},
+      username: '',
+      email: ''
+    });
+  }
+
   render() {
     return (
       <div>
@@ -85,12 +121,16 @@ class App extends Component {
                     <Form
                       formType={'Register'}
                       formData={this.state.formData}
+                      handleUserFormSubmit={this.handleUserFormSubmit}
+                      handleFormChange={this.handleFormChange}
                     />
                   )} />
                   <Route exact path='/login' render={() => (
                     <Form
                       formType={'Login'}
                       formData={this.state.formData}
+                      handleUserFormSubmit={this.handleUserFormSubmit}
+                      handleFormChange={this.handleFormChange}
                     />
                   )} />                            
                 </Switch>
