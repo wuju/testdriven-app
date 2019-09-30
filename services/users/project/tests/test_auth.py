f1d878a2b79b1ab384b847a735420398400c52a1
Woo Joo Lee <wuju.zero@gmail.com>
2019-09-30 18:35:48 +1300

    Fix code style with flake8

--- services/users/project/tests/test_auth.py
+++ services/users/project/tests/test_auth.py
@@ -1,11 +1,8 @@
 import json
 import unittest
-import time
 
 from flask import current_app
 
-from project import db
-from project.api.models import User
 from project.tests.base import BaseTestCase
 from project.tests.utils import add_user
 
