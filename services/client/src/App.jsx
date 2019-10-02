3dd6afeacde9e15e1b5ff5f3f9737f5a57eb491c
Woo Joo Lee <wuju.zero@gmail.com>
2019-10-02 16:03:40 +1300

    Add active and admin flag to User list and User Status view

--- services/client/src/App.jsx
+++ services/client/src/App.jsx
@@ -3,7 +3,6 @@ import axios from 'axios';
 import { Route, Switch } from 'react-router-dom';
 
 import UsersList from './components/UsersList';
-import AddUser from './components/AddUser';
 import About from './components/About';
 import NavBar from './components/NavBar';
 import Form from './components/Form';
@@ -117,19 +116,7 @@ class App extends Component {
                 <br />
                 <Switch>
                   <Route exact path="/" render={() => (
-                    <div>
-                      <h1 className="title is-1">All Users</h1>
-                      <hr /><br />
-                      <AddUser
-                          username={this.state.username}
-                          email={this.state.email}
-                          addUser={this.addUser}
-                          handleChange={this.handleChange}
-                        />
-                      <br></br>
-                      <UsersList users={this.state.users} />
-                      <br/>
-                    </div>
+                    <UsersList users={this.state.users} />
                   )} />
                   <Route exact path='/register' render={() => (
                     <Form
